e1a89cf211d8561cabc6f3b7538f04adeb2469ca
Agustin Olivarez <agustinolivarez29@gmail.com>
2022-06-05 18:32:01 -0300

    preparo entrega de simulador interactivo

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-var tabla = prompt("Indique que tabla desea");
-
-for (var i=1; i<11; i++) {
- document.write(tabla + "x"+ i+ "=" + (tabla*i) + "<br>");
+function calcularTotal() {
+  var valore= parseInt(document.getElementById("valor").value);
+  var resultado = 0;
+  var Argentina = 0.35;
+  var Chile = 0.19;
+  var Brasil = 0.18;
+  var Uruguay = 0.22;
+  if(document.getElementById ("uno").checked){
+      resultado = (valore * Argentina) + valore;
+      alert("El valor total del juego sera: U$D" + resultado);
+  }
+  else if (document.getElementById("dos").checked){
+      resultado = (valore * Chile) + valore;
+      alert("El valor total del juego sera: U$D" + resultado);
+  }
+  else if (document.getElementById("tres").checked){
+      resultado = (valore * Brasil) + valore;
+      alert("El valor total del juego sera: U$D" + resultado); 
+  }
+  else if (document.getElementById("cuatro").checked){
+    resultado = (valore * Uruguay) + valore;
+    alert("El valor total del juego sera: U$D" + resultado); 
 }
+ else{
+     alert("Tienes que seleccionar un pais")
+ }
+}
+
